a262e145bbb032fb6158dab2f9dece8deaf3a361
Koolkan <75653679+Koolkan0709@users.noreply.github.com>
2021-07-27 16:22:16 +0530

    Add files via upload

---
+++ Particle.js
@@ -0,0 +1,29 @@
+class Paritcle {
+    constructor(x, y) {
+        var options = {
+            restitution: 1,
+            friction: 0,
+            isStatic:true
+        }
+        this.r = 10;
+        this.body = Bodies.circle(x, y, this.r, options);
+        World.add(world, this.body);
+    }
+    display() {
+
+        var pos = this.body.position;
+        var angle = this.body.angle;
+
+        push();
+        this.color=color(random(0,255), random(0,255), random(0,255));
+        translate(pos.x, pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        noStroke();
+        fill("white");
+        ellipseMode(RADIUS);
+        ellipse(0,0,this.r,this.r);
+        pop();
+    }
+
+};
